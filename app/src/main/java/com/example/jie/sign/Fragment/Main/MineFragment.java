be01c7672b43eeea7f85523269ed54d99336eae6
ljtlcj <1521826348@qq.com>
2018-10-23 22:36:47 +0800

    提交人：chujie 提交内容：activity_login.xml

--- app/src/main/java/com/example/jie/sign/Fragment/Main/MineFragment.java
+++ app/src/main/java/com/example/jie/sign/Fragment/Main/MineFragment.java
@@ -1,19 +1,33 @@
 package com.example.jie.sign.Fragment.Main;
 
-import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.jie.sign.BaseTemplate.BaseLazyFragment;
 import com.example.jie.sign.CustomView.FastMenuBar;
-
 import com.example.jie.sign.R;
+import com.example.jie.sign.View.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 /**
  * Created by Diko on 2018/1/13.
  */
 public class MineFragment extends BaseLazyFragment implements FastMenuBar.onMenuBarClickListener {
+
+    @BindView(R.id.Ln_new)
+    LinearLayout LnNew;
+    @BindView(R.id.btn_back)
+    Button btnBack;
+    Unbinder unbinder;
 
     @Override
     public int getLayoutId() {
@@ -27,7 +41,8 @@ public class MineFragment extends BaseLazyFragment implements FastMenuBar.onMenu
 
     @Override
     public void initListener() {
-
+        btnBack.setOnClickListener(this);
+        LnNew.setOnClickListener(this);
     }
 
     @Override
@@ -37,11 +52,34 @@ public class MineFragment extends BaseLazyFragment implements FastMenuBar.onMenu
 
     @Override
     public void processClick(View v) {
-
+        switch (v.getId()){
+            case R.id.btn_back:
+                startActivity(LoginActivity.class);
+                break;
+            case R.id.Ln_new:
+                Toast.makeText(getActivity(), "当前为最先版本", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     public void onMenuBarClick(FastMenuBar view) {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
